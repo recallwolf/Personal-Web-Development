11081074f5d9a746483e27110aa5103cce6c947f
recallwolf <826628485@qq.com>
2017-05-03 06:07:37 +0800

    be ready

--- home/home/settings.py
+++ home/home/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k-oq9c@&5_7vx9b=x6-k!e%=%v9b^f7co*s6t-(hefsbr41al3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.rwnexus.com']
 
 
 # Application definition
