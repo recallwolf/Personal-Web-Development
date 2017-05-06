4e4de07761acaae76faf0170e93f04c890ad121e
recallwolf <826628485@qq.com>
2017-05-07 00:58:43 +0800

    fix #contact bug

--- Flat for Website/home/blog/static/blog/js/animate.js
+++ Flat for Website/home/blog/static/blog/js/animate.js
@@ -1,15 +1,19 @@
-$("document").ready(function(){
+$(document).ready(function(){
 	$(".nav a[href='#about']").click(function(){
         $("html,body").animate({
-		scrollTop:$(".pcover").offset().top},800);
+		scrollTop:$(".pcover").offset().top},1000);
 	});
 	$(".nav a[href='#project']").click(function(){
         $("html,body").animate({
-		scrollTop:$(".pcover2").offset().top},800);
+		scrollTop:$(".pcover2").offset().top},1000);
 	});
 	$(".nav a[href='#memorabilia']").click(function(){
         $("html,body").animate({
-		scrollTop:$(".pcover3").offset().top},800);
+		scrollTop:$(".pcover3").offset().top},1000);
+	});
+	$(".nav a[href='#contact']").click(function(){
+        $("html,body").animate({
+		scrollTop:$(".pcover5").offset().top},1000);
 	});
 });
 
