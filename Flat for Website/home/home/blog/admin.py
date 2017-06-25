7dfa3d071f601b89321b0f626e1a49c3425d08b9
recallwolf <826628485@qq.com>
2017-06-25 19:14:38 +0800

    delete extra file
    delete extra file

--- Flat for Website/home/home/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models 
-
-# Register your models here.
-class MessageAdmin(admin.ModelAdmin):
-	list_display = ('name', 'email', 'comments')
-
-admin.site.register(models.Article)
-admin.site.register(models.Message, MessageAdmin)
